503b1445c04bfd1d1998069433132c4b9257b26d
Olivi3rCar <mathias.carvalho@efrei.net>
2024-12-06 11:13:03 +0100

    Complete addition of load_riddles, display_question in pere_fouras_challenge.py Incomplete addition of pere_fouras_riddles in same file Complete addition of lowercase function in utility_functions.py

--- utility_functions.py
+++ utility_functions.py
@@ -1,2 +1,21 @@
 """file containing utility functions"""
 
+def lowercase(s : str) -> str :
+    """
+    Returns the lowercase version of a string
+    :param s: string to be lowercased
+    :return: lowercase version of the string
+    """
+    # instantiation of string to be returned
+    ns = ""
+
+    # iteration over s
+    for c in s:
+        # test of position of current char relative to uppercase bounds (A & Z)
+        if ord('A') <= ord(c) <= ord('Z'):
+            # concatenation of lowercase-converted current char to string
+            ns += chr(ord(c) + (ord('a') - ord('A')))
+        else :
+            # concatenation of current car to string
+            ns += c
+    return ns
